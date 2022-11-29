0a0f4b126295d2fa5f672518f1770dd64d71ac72
Ihor Smyk <ismyk256@gmail.com>
2022-11-29 23:46:55 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -19,13 +19,13 @@ refs.input.addEventListener('input', e => {
   });
 });
 
-//////////////////////////////////////////
-
 function renderCountries(data) {
   if (data.length === 1) {
     refs.info.innerHTML = infoCountries(data);
+    refs.list.innerHTML = "";
   } else if (data.length > 1 && data.length <= 10) {
     refs.list.innerHTML = listCountries(data);
+    refs.info.innerHTML = "";
   } else {
     //////////////  to do
   }
@@ -39,8 +39,7 @@ function infoCountries(data) {
     <h1 style="display: inline;">${name.official}</h1>
     <p><strong>Capital:</strong> ${capital}</p>
     <p><strong>Population:</strong> ${population}</p>
-    <p><strong>Languages:</strong> ${Object.values(languages)}</p>
-  `
+    <p><strong>Languages:</strong> ${Object.values(languages)}</p> `
     )
     .join('');
 }
@@ -49,10 +48,10 @@ function listCountries(data) {
   return data
     .map(
       ({ name, flags }) =>
-        ` <li>
-    <img src="${flags.png}" alt="country-flag" width="40px"/>
-    <p style="display: inline;">${name.official}</p>
-  </li>`
+     `<li style="margin-bottom: 13px">
+        <img src="${flags.png}" alt="country-flag" width="40px"/>
+        <p style="display: inline;">${name.official}</p>
+     </li> `
     )
     .join('');
 }
