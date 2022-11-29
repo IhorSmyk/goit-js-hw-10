78fc3151a901b4d3e2897524b8a54bc6f72e9383
Ihor Smyk <ismyk256@gmail.com>
2022-11-29 22:21:09 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const DEBOUNCE_DELAY = 300;
 
 const refs = {
   input: document.querySelector('input'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
 };
+
 
 refs.input.addEventListener('input', e => {
   e.preventDefault();
@@ -17,3 +20,57 @@ refs.input.addEventListener('input', e => {
   });
 });
 
+//////////////////////////////////////////
+
+function renderCountries(data) {
+  if (data.length === 1) {
+    const markInfo = infoCountries(data);
+    refs.info.innerHTML = markInfo;
+  } else {
+    const markList = listCountries(data);
+    refs.list.innerHTML = markList;
+  }
+}
+
+function infoCountries(data) {
+  let markup = data
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        ` <img src="${flags.png}" alt="country-flag" />
+    <h1>${name}</h1>
+    <p>Capital: ${capital}</p>
+    <p>Population: ${population}</p>
+    <p>Languages: ${Object.values(languages)}</p>
+  `
+    )
+    .join('');
+}
+
+function listCountries(data) {
+  let markup = data
+    .map(
+      ({ name, flags }) =>
+        ` <li>
+    <img src="${flags.png}" alt="country-flag" />
+    <p>${name}</p>
+  </li>
+  <li>
+    <img src="${flags.png}" alt="country-flag" />
+    <p>${name}</p>
+  </li>
+  <li>
+    <img src="${flags.png}" alt="country-flag" />
+    <p>${name}</p>
+  </li>
+  <li>
+    <img src="${flags.png}" alt="country-flag" />
+    <p>${name}</p>
+  </li>
+  `
+    )
+    .join('');
+}
+
+renderCountries(data);
+
+fetchCountries(name);
